87f801a19d8a355342a5316ccff55956500365ee
Daniel Silva <daniel.silva@onlineapp.com.br>
2022-05-16 14:44:29 -0300

    project fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {path:'', pathMatch:'full', redirectTo:'home'},
-  {path:'home', loadChildren: () => import('./home/home/home.module').then((m) => m.HomeModule)},
+  {path:'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)},
   {path:'task', loadChildren: () => import('./task/task.module').then((m) => m.TaskModule)},
 ];
 
